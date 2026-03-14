eeacad8234168057e6bb73a0b00846f13a2d4841
Kayla <118627199+KaylaML@users.noreply.github.com>
2026-03-14 20:50:57 +0000

    created a simple header bar

--- app/(dashboards)/layout.tsx
+++ app/(dashboards)/layout.tsx
@@ -1,10 +1,14 @@
+import Header from "../components/layout/Header";
 import Sidebar from "../components/layout/Sidebar";
 
 export default function DashboardLayout({children}: {children: React.ReactNode}) {
     return (
-        <div className="flex">
-            <Sidebar role="member" />
-            <main>{children}</main>
+        <div>
+            <Header />
+            <div className="flex">
+                <Sidebar role="member" />
+                <main>{children}</main>
+            </div>
         </div>
     )
 }
